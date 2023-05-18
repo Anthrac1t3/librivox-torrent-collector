d27b6d5084ea5421eabd6629d8f3c22421b03a77
Anthrac1t3 <coleholub@protonmail.com>
2023-05-17 22:25:07 -0500

    Initial commit

---
+++ driver.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+import time
+
+def downloadTorrent():
+    pass
+
+def processCatalogResult():
+    pass
+
+browser = webdriver.Firefox()
+
+browser.get('https://librivox.org/search?primary_key=0&search_category=title&search_page=1&search_form=get_results')
+
+time.sleep(5)
+
+bookList = browser.find_elements(By.CLASS_NAME, "catalog-result")
+
+bookList = WebDriverWait(browser, 20).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "catalog-result")))
+
+browser.quit()
