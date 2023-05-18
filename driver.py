f89975bb5611d0ca022ba72a7fface17af2823d1
Anthrac1t3 <coleholub@protonmail.com>
2023-05-18 10:27:45 -0500

    Some progress on elemnt selection

--- driver.py
+++ driver.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-
 import time
 
 def downloadTorrent():
@@ -16,10 +15,13 @@ browser = webdriver.Firefox()
 
 browser.get('https://librivox.org/search?primary_key=0&search_category=title&search_page=1&search_form=get_results')
 
-time.sleep(5)
+#time.sleep(5)
 
-bookList = browser.find_elements(By.CLASS_NAME, "catalog-result")
+#bookList = browser.find_elements(By.CLASS_NAME, "catalog-result")
 
-bookList = WebDriverWait(browser, 20).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "catalog-result")))
+bookList = WebDriverWait(browser, 20).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".result-data a [href]")))
+
+for book in bookList:
+    print(book.get_attribute('href'))
 
 browser.quit()
