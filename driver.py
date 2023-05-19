cd19b6ac884cb5b2923979d733aa402822c19b9c
Anthrac1t3 <coleholub@protonmail.com>
2023-05-19 00:21:31 -0500

    Working

--- driver.py
+++ driver.py
@@ -1,27 +1,52 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-import time
+from selenium.webdriver.firefox.options import Options
 
-def downloadTorrent():
-    pass
+downloadLocation = r"\\10.28.28.4\Torrents\AudioBooks"
 
-def processCatalogResult():
-    pass
+options = Options()
+options.set_preference("browser.download.folderList", 2)
+options.set_preference("browser.download.manager.showWhenStarting", False)
+options.set_preference("browser.download.dir", downloadLocation)
+options.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/x-gzip")
 
-browser = webdriver.Firefox()
+browser = webdriver.Firefox(options=options)
 
-browser.get('https://librivox.org/search?primary_key=0&search_category=title&search_page=1&search_form=get_results')
+currentPage = 1
 
-#time.sleep(5)
+browser.get(f"https://librivox.org/search?primary_key=0&search_category=title&search_page={currentPage}&search_form=get_results")
 
-#bookList = browser.find_elements(By.CLASS_NAME, "catalog-result")
+lastPage = int(WebDriverWait(browser, 30).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'page-number')))[-1].get_attribute('data-page_number'))
 
-bookList = WebDriverWait(browser, 20).until(EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".result-data a [href]")))
+while True:
+    #Create a list for holding the URLs of the books on the page
+    bookList = []
 
-for book in bookList:
-    print(book.get_attribute('href'))
+    #Grab a list of all the catalog result objects on the page load. These object represent the books, their links and info
+    resultList = WebDriverWait(browser, 30).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'catalog-result')))
 
+    #Grab the book URLs from each one of those objects and append it to the bookList
+    for result in resultList:
+        bookList.append(result.find_element(By.TAG_NAME, 'a').get_attribute('href'))
+
+    #Now process the book list
+    for book in bookList:
+        #Get the load the book's URL into the browser
+        browser.get(book)
+        #When the "Torrent" button loads, click it and start the download
+        WebDriverWait(browser, 30).until(EC.visibility_of_element_located((By.XPATH, "//*[text()='Torrent']"))).click()
+
+    #If we are on the last page of the catalog then we can drop out of the main loop
+    if currentPage == lastPage:
+        break
+
+    #Increment the page count when we are done with each page
+    currentPage += 1
+
+    #Get the next page of the catalog
+    browser.get(f"https://librivox.org/search?primary_key=0&search_category=title&search_page={currentPage}&search_form=get_results")
+
+#Close up the browser
 browser.quit()
